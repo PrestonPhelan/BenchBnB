471d5d029444accbb58fffaaf8362ca0339ba7d0
PrestonPhelan <preston.phelan@gmail.com>
2017-07-05 18:25:39 -0700

    User db migration

--- db/migrate/20170704232011_create_users.rb
+++ db/migrate/20170704232011_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, unique: true, presence: true
-      t.string :password_digest, presence: true
-      t.string :session_token, unique: true
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
+
+    add_index :users, :username, unique: true
   end
 end
